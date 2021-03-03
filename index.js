b7677b9c8bf0fa80b293458d25e26c131aaa4532
Michal Husak <HusakM1@basfad.basf.net>
2021-03-03 20:52:56 +0100

    google translations

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const puppeteer = require('puppeteer');
 const ObjectsToCsv = require('objects-to-csv');
-const express = require('express')
+const translate = require('@vitalets/google-translate-api');
+const express = require('express');
 const app = express();
 
 app.get('/html', async function (req, res) {
     const url = req.query.url;
-    const data = await scrape(url);
+    const reviews = await scrape(url);
     res.contentType = 'html';
-    res.send(reviewsToHtml(data));
+    await translateReviews(reviews);
+    res.send(reviewsToHtml(reviews));
 });
 
 app.get('/csv', async function (req, res) {
     const url = req.query.url;
     const productHandle = req.query.productHandle;
-    const data = await scrape(url);
+    const reviews = await scrape(url);
+    await translateReviews(reviews);
     res.contentType = 'text/csv';
     res.attachment('export.csv');
-    res.send(await reviewsToCsv(data, productHandle));
+    res.send(await reviewsToCsv(reviews, productHandle));
 });
 
 function reviewsToHtml(reviews) {
@@ -32,6 +35,9 @@ function reviewsToHtml(reviews) {
         text += review.body
         text += '</p>'
         text += '\r\n';
+        text += review.original_body
+        text += '</p>'
+        text += '\r\n';
         text += review.imageUrl ? `<a href="${review.imageUrl}">${review.imageUrl}</a>` : '';
         text += '\r\n';
         text += '</br>';
@@ -44,6 +50,24 @@ async function reviewsToCsv(reviews, productHandle) {
     reviews = reviews.map(review => ({...review, product_handle: productHandle}));
     const csv = new ObjectsToCsv(reviews);
     return await csv.toString(true, false);
+}
+
+async function translateReviews(reviews) {
+    return Promise.all(reviews.map(review => translateReview(review)));
+}
+
+async function translateReview(review) {
+    return translate(
+        review.body,
+        {
+            from: 'en',
+            to: 'de'
+        }
+    ).then(res => {
+        review.body = res.text;
+    }).catch(err => {
+        console.error(err);
+    });
 }
 
 async function scrape(shopUrl) {
@@ -64,13 +88,13 @@ async function scrape(shopUrl) {
     let data = await frame.evaluate(async () => {
 
         class Review {
-            constructor(author, body, imageUrl, rating, product_handle ) {
+            constructor(author, body, original_body, imageUrl, rating, product_handle ) {
               this.author = author;
               this.body = body;
+              this.original_body = original_body;
               this.imageUrl = imageUrl;
               this.rating = rating;
               this.product_handle = product_handle;
-        
             }
         }
 
@@ -80,7 +104,6 @@ async function scrape(shopUrl) {
         const numOfReviews = parseInt(dataUrlParams.get('total'));
         const reviewsPerClick = 5;
         const numOfPages = Math.floor(numOfReviews / reviewsPerClick);
-        const maxClicksPerPage = 10;
         const rawReviews = Array.from(document.getElementsByClassName('grid-item-wrap'));
 
         for (let index = 0; index < numOfPages; index++) {
@@ -98,6 +121,7 @@ async function scrape(shopUrl) {
                 const img = rawReview.getElementsByClassName('item-img')[0];
                 return new Review(
                     rawReview.querySelector('.block.title').textContent,
+                    rawReview.querySelector('.pre-wrap.main-text.action').textContent,
                     rawReview.querySelector('.pre-wrap.main-text.action').textContent,
                     img ? img.firstChild.src.replace('.jpg', '_mid.jpg') : null,
                     5
